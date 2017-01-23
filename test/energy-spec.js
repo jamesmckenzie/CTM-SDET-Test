acafc306cfcb56a148020aecfaa3135b64f5dfd2
jamesmckenzie <jamesmck1987@gmail.com>
2017-01-23 10:19:58 +0000

    test skeleton

--- test/energy-spec.js
+++ test/energy-spec.js
@@ -1,5 +1,33 @@
 describe('Compare the Market Energy Journey', function () {
-    it('should work', function () {
-        browser.get('https://energy.comparethemarket.com/energy/v2/?AFFCLIE=TSTT')
+    beforeEach(function () {
+        browser.get('https://energy.comparethemarket.com/energy/v2/?AFFCLIE=TSTT');
     });
+
+    afterEach(function () {
+        browser.manage().deleteAllCookies();
+    });
+    
+    describe('User Journey 1', function () {
+        it('should work', function () {
+
+            var yourSupplierDetails = {
+            }
+
+            var yourEnergyDetails = {
+            }
+
+            var yourDetails = {
+            }            
+        });        
+    });
+
+    describe('User Journey 2', function () {
+        it('should work', function () {
+        });        
+    });
+    
+    describe('User Journey 3', function () {
+        it('should work', function () {
+        });        
+    });    
 });
